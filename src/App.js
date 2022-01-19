c26fed56075a45cc1b796a1ae21ef26aa260d3ac
yijoon009 <74906042+yijoon009@users.noreply.github.com>
2022-01-19 18:01:25 +0900

    Reference

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import { useEffect, useState } from "react";
-
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
-};
+import { useRef } from "react";
 
 const App = () => {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("home"), 5000);
+  const potato = useRef();
+  // setTimeout(() => console.log(potato.current), 5000);
+  setTimeout(() => potato.current.focus(), 5000);
   return (
     <div className="App">
       <h2>Hello!</h2>
+      <input ref={potato} placeholder="la" />
     </div>
   );
 };
